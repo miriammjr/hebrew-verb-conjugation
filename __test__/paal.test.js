37042faad1e62c200b8b9f00a02549411f9a41d2
onamonapia <mirrundell@gmail.com>
2025-05-22 22:53:10 -0700

    סיימתי את "פעל שלמים (אפעול)" להווה

--- __test__/paal.test.js
+++ __test__/paal.test.js
@@ -1,30 +1,43 @@
-const Paal = require("../paal");
+const PaalShlemimEfol = require("../paal-shlemim-efol");
+const test1 = new PaalShlemimEfol("ר", "ג", "ס");
+const test2 = new PaalShlemimEfol("a", "b", "c");
 
 test("tests that a is the first input in a 3-letter root", () => {
-  const test1 = new Paal("a", "b", "c");
-  expect(test1.root1).toBe("a");
+  expect(test2.root1).toBe("a");
 });
 test("tests that b is the second input in a 3-letter root", () => {
-  const test1 = new Paal("a", "b", "c");
-  expect(test1.root2).toBe("b");
+  expect(test2.root2).toBe("b");
 });
 
 test("tests that c is the third input in a 3-letter root", () => {
-  const test1 = new Paal("a", "b", "c");
-  expect(test1.root3).toBe("c");
+  expect(test2.root3).toBe("c");
 });
 
 // testing if hebrew letters work lol
 test("tests that a is the first input in a 3-letter root", () => {
-  const test1 = new Paal("ר", "ג", "ס");
   expect(test1.root1).toBe("ר");
 });
 test("tests that b is the second input in a 3-letter root", () => {
-  const test1 = new Paal("ר", "ג", "ס");
   expect(test1.root2).toBe("ג");
 });
 
 test("tests that c is the third input in a 3-letter root", () => {
-  const test1 = new Paal("ר", "ג", "ס");
   expect(test1.root3).toBe("ס");
+});
+
+// testing verb conjugations
+test("tests present maculine single", () => {
+  expect(test1.presentMascSing()).toBe("סוֹגֵר");
+});
+
+test("tests present feminine singular", () => {
+  expect(test1.presetFemSing()).toBe("סוֹגֶרֶת");
+});
+
+test("tests present masculine plural", () => {
+  expect(test1.presentMascPlural()).toBe("סוֹגְרִים");
+});
+
+test("tests present feminine plural", () => {
+  expect(test1.presentFemPlural()).toBe("סוֹגְרוֹת");
 });
--- paal.js
+++
@@ -1,9 +0,0 @@
-class Paal {
-  constructor(a, b, c) {
-    this.root1 = a;
-    this.root2 = b;
-    this.root3 = c;
-  }
-}
-
-module.exports = Paal;
